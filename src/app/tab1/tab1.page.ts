a1f646dbd9abb8bebf14cfff2707297ac51d0980
Vitor Lopes Nunes <vitorlopes040805@gmail.com>
2023-03-18 11:13:29 -0300

    ICT

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,18 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotateState', [
+      state('default', style({transform: 'rotatey(0'})),
+      state('default', style({transform: 'rotatey(-720deg'})),
+      transition('rotate => default', animate('2395ms ease-out')),
+      transition('default => rotate', animate('2395ms ease-in'))
+    ])
+  ]
 })
 export class Tab1Page {
 
@@ -12,16 +21,25 @@ export class Tab1Page {
   logo = 'assets/logo.png';
   image = this.logo;
   info = 'Clique no botão para jogar!';
+  state = 'default';
 
   constructor() {}
 
-  jogarMoeda() {
-    if(Math.random() < 0.5){
+  jogarMoeda(){
+    this.info = 'Girando...';
+    this.image = this.logo;
+    this.state = this.state === 'default' ? 'rotate' : 'default';
+
+    setTimeout(() => {
+    if(Math.random() < 0.5 ){
       this.image = this.cara;
       this.info = "Cara!"
-    }else{
+    }
+    else{
       this.image = this.coroa;
       this.info = "Coroa!"
     }
-  }
+  }, 2000)};
+
+
 }
